fc7e497800c0d62b4438554f13f65396d4aa760a
dong6662 <liuihaodong666@gmail.com>
2025-05-25 13:05:00 +0800

    border_width, border_height

--- src/node_crop_image_by_mask/nodes.py
+++ src/node_crop_image_by_mask/nodes.py
@@ -1,4 +1,6 @@
 from inspect import cleandoc
+
+
 class MaskCropperNode:
     def __init__(self):
         pass
@@ -23,24 +25,26 @@ class MaskCropperNode:
             "required": {
                 "image": ("IMAGE", {"tooltip": "This is an image"}),
                 "mask": ("MASK", {"tooltip": "mask that used to crop image"}),
+                "border_width": ("INT", {"tooltip": "border width"}),
+                "border_height": ("INT", {"tooltip": "border height"})
             },
         }
 
     RETURN_TYPES = ("IMAGE", 'MASK')
-    #RETURN_NAMES = ("image_output_name",)
+    # RETURN_NAMES = ("image_output_name",)
     # DESCRIPTION = cleandoc(__doc__)
     FUNCTION = "do_crop_by_mask"
 
-    #OUTPUT_NODE = False
-    #OUTPUT_TOOLTIPS = ("",) # Tooltips for the output node
+    # OUTPUT_NODE = False
+    # OUTPUT_TOOLTIPS = ("",) # Tooltips for the output node
 
     CATEGORY = "image"
 
-    def do_crop_by_mask(self, image, mask):
+    def do_crop_by_mask(self, image, mask, border_width=0, border_height=0):
         # image [B,H,W,3] -> [B,3,H,W]
         # mask [B,H,W]
-        print('mask shape',mask.shape)
-        print('image shape',image.shape)
+        print('mask shape', mask.shape)
+        print('image shape', image.shape)
         mask = mask[0]
         # print(mask)
         left, right, top, bottom = mask.shape[1], 0, mask.shape[0], 0
@@ -54,14 +58,16 @@ class MaskCropperNode:
                     bottom = max(bottom, i)
 
         print(left, right, top, bottom)
+
+        top, bottom = max(0, top-border_height), min(mask.shape[0], bottom+border_height)
+        left, right = max(0, left-border_width), min(mask.shape[1], right+border_width)
+
         cropped_mask = mask[top:bottom, left:right]
         cropped_mask = cropped_mask.float()
 
         cropped_image = image[:, top:bottom, left:right, :]
         cropped_image = cropped_image.float()
         return cropped_image, cropped_mask
-
-
 
     """
         The node will always be re executed if any of the inputs change but
@@ -71,8 +77,8 @@ class MaskCropperNode:
         This method is used in the core repo for the LoadImage node where they return the image hash as a string, if the image hash
         changes between executions the LoadImage node is executed again.
     """
-    #@classmethod
-    #def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
+    # @classmethod
+    # def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
     #    return ""
 
 
